708dcbcc6e783e4eb72de4e229cc404702ddde2e
huahuahu <guotenghu@gmail.com>
2023-04-01 15:53:14 +0800

    clipboard: Support dark mode

--- apps/clipboard-inspector/src/app/app.tsx
+++ apps/clipboard-inspector/src/app/app.tsx
@@ -6,9 +6,25 @@ import Box from "@mui/material/Box/Box";
 import Button from "@mui/material/Button/Button";
 import ContentPasteGoIcon from "@mui/icons-material/ContentPasteGo";
 import Typography from "@mui/material/Typography/Typography";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline, createTheme, useMediaQuery } from "@mui/material";
 
 export function App() {
   const [pastedText, setPastedText] = React.useState<string>("");
+
+  // get theme using useMediaQuery
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  // create mui theme based on prefersDarkMode
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
 
   const onPaste = useCallback(() => {
     navigator.clipboard
@@ -22,41 +38,45 @@ export function App() {
   }, [setPastedText]);
 
   return (
-    <Container
-      maxWidth="md"
-      sx={{
-        flexGrow: 1,
-        display: "flex",
-        flexDirection: "column",
-      }}
-    >
-      <Box
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+
+      <Container
+        maxWidth="md"
         sx={{
-          bgcolor: "#cfe8fc",
-          height: "100%",
+          flexGrow: 1,
           display: "flex",
           flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-          flexGrow: 1,
         }}
       >
-        <Button
-          onClick={onPaste}
-          size="large"
-          variant="contained"
-          endIcon={<ContentPasteGoIcon />}
+        <Box
+          sx={{
+            bgcolor: "#cfe8fc",
+            height: "100%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            flexGrow: 1,
+          }}
         >
-          Paste
-        </Button>
+          <Button
+            onClick={onPaste}
+            size="large"
+            variant="contained"
+            endIcon={<ContentPasteGoIcon />}
+          >
+            Paste
+          </Button>
 
-        {pastedText && (
-          <Typography variant="subtitle1" component="h2">
-            {pastedText}
-          </Typography>
-        )}
-      </Box>
-    </Container>
+          {pastedText && (
+            <Typography variant="subtitle1" component="h2">
+              {pastedText}
+            </Typography>
+          )}
+        </Box>
+      </Container>
+    </ThemeProvider>
   );
 }
 
