1e4f72a0226a734f58e46f79f23226b78ff54dc8
huahuahu <guotenghu@gmail.com>
2023-04-06 21:00:29 +0800

    Add sinin demo

--- apps/web-learn/src/app/app.tsx
+++ apps/web-learn/src/app/app.tsx
@@ -1,7 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Container, Link, Typography } from "@mui/material";
 import React from "react";
-import { Outlet } from "react-router-dom";
 import styles from "./app.module.css";
 
 export function App() {
