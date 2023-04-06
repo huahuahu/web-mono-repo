2239fa63863a7d74712443d234b01f928235c933
tiger <guotenghu@gmail.com>
2023-04-06 18:56:02 +0800

    create vite project (#34)

---
+++ apps/web-learn/src/app/app.tsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Typography } from "@mui/material";
+import React from "react";
+import styles from "./app.module.css";
+
+export function App() {
+  return <Typography>Welcome</Typography>;
+}
+
+export default App;
