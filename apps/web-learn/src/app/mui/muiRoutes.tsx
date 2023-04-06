5f023dbc91c752b29a7472faa916e76e2be22d6b
huahuahu <guotenghu@gmail.com>
2023-04-06 22:07:55 +0800

    fix build

--- apps/web-learn/src/app/mui/muiRoutes.tsx
+++ apps/web-learn/src/app/mui/muiRoutes.tsx
@@ -1,7 +1,7 @@
 import { RouteObject } from "react-router-dom";
 import React from "react";
 import { MUIEntry } from "./muiEntry";
-import SignIn from "./Signin";
+import SignIn from "./SignIn";
 
 export const muiRoutes: RouteObject[] = [
   {
