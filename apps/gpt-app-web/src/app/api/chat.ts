043c6d424a08dbaa5561ee219929d2f3411f0497
tiger <guotenghu@gmail.com>
2023-05-02 15:06:27 +0800

    gpt: use azure function to answer question (#48)

--- apps/gpt-app-web/src/app/api/chat.ts
+++ apps/gpt-app-web/src/app/api/chat.ts
@@ -1,12 +1,12 @@
 // create an async function to call the api using fetch
 export const getChat = async (animal: string) => {
-  const response = await fetch(`/api/chat`, {
+  const response = await fetch(`/api/open-ai-proxy`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ animal }),
+    body: JSON.stringify({ question: animal }),
   });
   const data = await response.json();
-  return data.result as string;
+  return data.choices[0].message.content as string;
 };
