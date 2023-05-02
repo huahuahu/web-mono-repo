0fcf27ea3e2d6ce4c855bb34e470db7132840256
huahuahu <guotenghu@gmail.com>
2023-05-02 08:50:48 +0800

    test: Add env for azure function

--- apps/clipboard-inspector/src/app/Components/PrivacyComponent/PrivacyComponent.tsx
+++ apps/clipboard-inspector/src/app/Components/PrivacyComponent/PrivacyComponent.tsx
@@ -6,12 +6,12 @@ import { LocalizedStringManager } from "../../Localization/localizedString";
 export function PrivacyComponent() {
   useEffect(() => {
     (async function () {
-      const text  = await( await fetch(`/api/clipboard-test`)).text();
+      const text  = await( await fetch(`http:localhost:3000/api/clipboard-test?name=haha`)).text();
       console.log(text);
     })();
   }, []);
 
-  
+
   return (
     <Container
       component="main"
