31628674e0260f552f08a6fcfb7a60b24ef189c1
huahuahu <guotenghu@gmail.com>
2023-05-02 09:54:12 +0800

    only clipboard change

--- azure-function/clipboard-test/index.ts
+++ azure-function/clipboard-test/index.ts
@@ -5,8 +5,8 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     const name = (req.query.name || (req.body && req.body.name));
     const testEnv = process.env.TEST_ENVIRONMENT;
     let responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully." + testEnv
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response." + testEnv;
+        ? "Hello,lalal " + name + ". This HTTP triggered function executed successfully." + testEnv
+        : "lalaal This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response." + testEnv;
     responseMessage += " " + process.env.TEST_ENVIRONMENT + "env";
     context.res = {
         // status: 200, /* Defaults to 200 */
